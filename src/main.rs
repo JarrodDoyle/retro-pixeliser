920da6103296bc45bdcfa4d66b0f8bc7b40ed170
Jarrod Doyle <thejarroddoyle@gmail.com>
2026-04-22 17:52:58 +0100

    feat: hardcoded image downscaling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use image::{ImageError, ImageReader, imageops::FilterType};
+
+const INPUT_PATH: &str = "./res/input.png";
+const OUTPUT_PATH: &str = "./res/output.png";
+const DOWNSCALE: u32 = 4;
+
+fn main() -> Result<(), ImageError> {
+    let image = ImageReader::open(INPUT_PATH)?.decode()?;
+    let output_image = image.resize(
+        image.width() / DOWNSCALE,
+        image.height() / DOWNSCALE,
+        FilterType::Nearest,
+    );
+
+    output_image.save(OUTPUT_PATH)?;
+    Ok(())
 }
